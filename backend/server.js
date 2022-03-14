7425ded959452b3cbf044ec4251aa8051f61339d
tosettil-polimi <info@tosettil.me>
2022-03-14 23:16:49 +0100

    Add dotenv, http-status deps

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('diofa')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
