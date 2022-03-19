9afa48d9874ebdd2adddbf33e3563fe8c78020da
tosettil-polimi <info@tosettil.me>
2022-03-19 16:52:23 +0100

    Fix scss and reformatting MarketplaceComponent, add vue source mapping

--- frontend/src/components/section/Marketplace/MarketplaceComponent.vue
+++ frontend/src/components/section/Marketplace/MarketplaceComponent.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="container-fluid marketplace">
-    <div class="container">
-      <div class="row title-marketplace">
-        <div class="col-12">
-          <TitleSectionComponent title="Marketplace" subtitle="All the NFTs opportunity"/>
-        </div>
+  <div class="container marketplace">
+    <div class="row title-marketplace">
+      <div class="col-12">
+        <TitleSectionComponent title="Marketplace" subtitle="All the NFTs opportunity"/>
       </div>
-      <div class="row">
-        <div class="col-sm-4">
-          <SliderComponent type="banner" :image="sliderAWSImage"
-                           :content="{ title: 'Awesome banner', subtitle: 'Testo super importante' }"/>
-        </div>
-        <div class="col-sm-4">
-          <SliderComponent type="main" :image="sliderAWSImage"
-                           :content="{ title: 'Top 15', subtitle: 'Jolly card' }"/>
-        </div>
-        <div class="col-sm-4">
-          <SliderComponent type="banner" :image="sliderAWSImage"
-                           :content="{ title: 'Awesome banner', subtitle: 'Testo super importante' }"/>
-        </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <SliderComponent type="banner" :image="sliderAWSImage"
+                         :content="{ title: 'Awesome banner', subtitle: 'Testo super importante' }"/>
+      </div>
+      <div class="col-sm-4">
+        <SliderComponent type="main" :image="sliderAWSImage"
+                         :content="{ title: 'Top 15', subtitle: 'Jolly card' }"/>
+      </div>
+      <div class="col-sm-4">
+        <SliderComponent type="banner" :image="sliderAWSImage"
+                         :content="{ title: 'Awesome banner', subtitle: 'Testo super importante' }"/>
       </div>
     </div>
   </div>
